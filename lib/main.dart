397bda78e5a6251084b9ae4fe17b3e73cad3c141
Sasen Tamrakar <sasen.tamrakar@yajtechnologies.com>
2023-12-09 16:23:12 +0545

    native splash remove called

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,19 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
